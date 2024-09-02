af0d7fafc517d74ee4ead556cacabd6ea628c372
MantY <110971384+manty3@users.noreply.github.com>
2024-09-02 09:10:40 +0530

    db commit

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose')
 const connectDB = async () => {
   try {
     mongoose.set('strictQuery',false);
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    const conn = await mongoose.connect(process.env.MONGODB_URI {
+      useNewUrlParser: true, // Use the new URL parser
+      useUnifiedTopology: true, // Use the new server discovery and monitoring engine
+      serverSelectionTimeoutMS: 30000, // Timeout after 30s instead of 10s
+    });
     console.log(`Database connected: ${conn.connection.host}`)
   }catch (err) {
     console.log(err)
