91c98f1ed3b0abffb9efb13d486afac539c22a8f
MantY <110971384+manty3@users.noreply.github.com>
2024-09-02 09:29:41 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -3,9 +3,11 @@ const connectDB = async () => {
   try {
     mongoose.set('strictQuery',false);
     const conn = await mongoose.connect(process.env.MONGODB_URI,{
-      useNewUrlParser: true, // Use the new URL parser
-      useUnifiedTopology: true, // Use the new server discovery and monitoring engine
-      serverSelectionTimeoutMS: 30000, // Timeout after 30s instead of 10s
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 30000, // Timeout after 30s
+      tlsAllowInvalidCertificates: true, // Allows invalid certificates (not recommended for production)
+      tlsAllowInvalidHostnames: true,
     });
     console.log(`Database connected: ${conn.connection.host}`)
   }catch (err) {
